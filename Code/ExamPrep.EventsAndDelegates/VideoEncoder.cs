383efc128526f7986eda58828c0b6e48ef88d783
azavarben <btiberi@azavar.com>
2017-10-30 20:07:59 -0400

    event stuff

---
+++ Code/ExamPrep.EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ExamPrep.EventsAndDelegates
+{
+    public class VideoEncoder
+    {
+        //1 - define a delegate
+        //2 - define an event based of the delegate
+        //3 - raise the event
+
+        #region Old School Way
+        //public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
+        //public event VideoEncodedEventHandler VideoEncoded;
+        #endregion
+
+        #region New Way
+        public EventHandler<VideoEventArgs> VideoEncoded;
+        #endregion
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding video....");
+            Thread.Sleep(3000);
+
+            OnVideoEncoded(video);
+        }
+
+        protected virtual void OnVideoEncoded(Video video)
+        {
+            if (VideoEncoded != null) //are there any subscribers?
+            {
+                VideoEncoded(this, new VideoEventArgs {
+                    Video = video
+                });
+            }
+        }
+    }
+}
---
+++ Code/ExampPrep.Threads.UI.ConsoleApp/Listing174.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampPrep.Threads.UI.ConsoleApp
+{
+    class Listing174
+    {
+        public static void JumpStatements()
+        {
+            int x = 3;
+
+            if (x == 3)
+            {
+                goto customlabel;
+            }
+
+            Console.WriteLine("This will not show.");
+
+            customlabel:
+                Console.WriteLine(x);
+
+            Console.ReadLine();
+        }
+    }
+}
